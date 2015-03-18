bb722e8ab940565b47b17681eba4e35ec8133617
Kyle S. Johnston <KyleSJohnston@users.noreply.github.com>
2015-03-18 01:15:28 -0400

    More code to get Wikipedia data.

--- R/MediaWiki.R
+++ R/MediaWiki.R
@@ -16,7 +16,7 @@
 #' 
 #' @example
 #' \dontrun{
-#' bosox <- MediaWikiSpecialExport("Boston_Red_Sox", offset="2015-01-01T04:39:58Z", limit=1)
+#' bosox <- MediaWikiSpecialExport("Boston_Red_Sox", offset="2015-01-01T04:39:58Z", limit=3)
 #' nchar(bosox)
 #' }
 #' 
@@ -34,11 +34,76 @@ MediaWikiSpecialExport <- function(pages, dir=c("asc", "desc"), offset=1,
   content(r, "text")
 }
 
+xmlTreeParse(bosox, asText=TRUE)
 
-## Function to extract the results
-# title
-# revisionid
-# revisionTime
-# contributorName
-# contributorId
-# text
+#' Data extraction class
+#' 
+#' Given the standard format of the Special:Export API function, this object
+#' can parse the results and produce a \code{data.frame}. This class is INCOMPLETE, 
+#' and does not return the right text yet.
+#' 
+#' This class was adapted from my answers to an earlier assignment in the 
+#' course.
+#' 
+#' @example
+#' \dontrun{
+#' parser <- MediaWikiSpecialExportParser$new()
+#' handler <- xmlEventParse(file=bosox, branches=parser$saxHandler(), asText=TRUE)
+#' revisions <- parser$getDetails()
+#' str(revisions)
+#' }
+#' 
+#' @export
+MediaWikiSpecialExportParser <- R6Class("MediaWikiSpecialExportParser",
+  public=list(
+    initialize = function() {
+    },
+    saxHandler = function() {
+       page <- function(context, node, attrs, ...) {
+         title <- xmlValue(xmlElementsByTagName(node, name="title")[[1]])
+         pageId <- xmlValue(xmlElementsByTagName(node, name="id")[[1]])
+         revisionNodes <- xmlElementsByTagName(node, name="revision")
+         for (revNode in revisionNodes) {
+           timestamp <- xmlValue(xmlElementsByTagName(revNode, name="timestamp")[[1]])
+           contributorNodes <- xmlElementsByTagName(revNode,name="contributor")
+           username <- NA
+           userId <- NA
+           if (length(contributorNodes) > 0) {
+             usernameNodes <- xmlElementsByTagName(
+               contributorNodes[[1]], name="username")
+             if (length(usernameNodes) > 0) {
+               username <- xmlValue(usernameNodes[[1]])
+             }
+             userIdNodes <- xmlElementsByTagName(
+               contributorNodes[[1]], name="id")
+             if (length(userIdNodes) > 0) {
+               userId <- xmlValue(userIdNodes[[1]])
+             }
+           }
+           text <- xmlValue(xmlElementsByTagName(revNode, name="text")[[1]])
+           dfRow <- list(title=title,
+                         pageId=pageId,
+                         timestamp=timestamp, 
+                         contributorId=userId, 
+                         contributorName=username,
+                         text=text)
+           private$addRevision(dfRow)
+         }
+       }
+       c(page=xmlParserContextFunction(page))
+     },
+     getDetails = function() {
+       private$details
+     }
+   ),
+   private=list(
+     details = NULL,
+     addRevision = function(dfRow) {
+       if (is.null(private$details)) {
+         private$details <- data.frame(dfRow, stringsAsFactors=FALSE)
+       } else {
+         private$details <- rbind(private$details, dfRow)
+       }
+     }
+   )
+)
