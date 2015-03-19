db34117e928b3c1c82721e3706ba4a6c92cef247
joejohnsonlive <joejohnsonlive@gmail.com>
2015-03-19 00:59:30 -0400

    Fixed xml parse test cases

--- tests/testthat/testXmlParse.R
+++ tests/testthat/testXmlParse.R
@@ -7,8 +7,6 @@ library(testthat)
 
 context("XML Parsing of local file")
 
-print(getwd())
-
 # Expect working dir is <package home>/tests/testthat:
 load("../../../evolvis/data/small.rda")
 parser <- MediaWikiSpecialExportParser()
@@ -17,9 +15,9 @@ revisions <- parser$details()
 
 test_that("XML parsed objects are as expected", {
   expect_is(revisions, "data.frame")
-  expect_equal(sum(dim(revisions) == c(6,6)), 2)
+  expect_equal(sum(dim(revisions) == c(3,6)), 2)
   expect_equal(revisions[1,"pageId"], "1283228")
-  expect_equal(sum(nchar(revisions[,6]) == c(716,701,701,710,701,701)), 6)
+  expect_equal(sum(nchar(revisions[,6]) == c(716,701,701)), 3)
 })
 
 ##
